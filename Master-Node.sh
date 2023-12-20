05a27ed64b5c8cd01b6777ae19d72a76c5d05a5e
Abhiraj Parthan <100773790+abhirajparthan@users.noreply.github.com>
2023-12-20 10:39:51 +0530

    Add files via upload
    Master-Node.sh file.

---
+++ Master-Node.sh
@@ -0,0 +1,69 @@
+
+
+#  Disable swap
+sudo swapoff -a
+sudo sed -i '/ swap / s/^/#/' /etc/fstab
+
+# Set up hostnames
+sudo hostnamectl set-hostname "master-node"
+
+# For using new hostname immediately.
+exec bash
+
+# Set up the IPV4 bridge on Master Node
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+
+# Load overlay and br_netfilter
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+# sysctl params required by setup, params persist across reboots
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+
+# Apply sysctl params without reboot
+sudo sysctl --system
+
+# Install kubelet, kubeadm, and kubectl Master Node
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl
+sudo mkdir /etc/apt/keyrings
+curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt install -y kubelet=1.26.5-00 kubeadm=1.26.5-00 kubectl=1.26.5-00
+
+# Install Docker
+sudo apt install docker.io
+sudo mkdir /etc/containerd
+sudo sh -c "containerd config default > /etc/containerd/config.toml"
+sudo sed -i 's/ SystemdCgroup = false/ SystemdCgroup = true/' /etc/containerd/config.toml
+sudo systemctl restart containerd.service
+sudo systemctl restart kubelet.service
+sudo systemctl enable kubelet.service
+
+
+# Initialize the Kubernetes cluster on the master node
+sudo kubeadm config images pull
+sudo kubeadm init --pod-network-cidr=10.10.0.0/16
+
+# To manage the cluster, you should configure kubectl on the master node. Create the .kube directory in your home folder and copy the cluster's admin configuration to your personal .kube directory. 
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+
+
+
+
+
+
+
+
+
+
