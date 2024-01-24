c724dd9df889a5cbf57cca91d5b50655b00852d6
Abhiraj Parthan <100773790+abhirajparthan@users.noreply.github.com>
2024-01-24 09:12:51 +0530

    Update Worker-Node.sh

--- Worker-Node.sh
+++ Worker-Node.sh
@@ -43,6 +43,7 @@ sudo mkdir /etc/apt/keyrings
 curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt install -y kubelet=1.26.5-00 kubeadm=1.26.5-00 kubectl=1.26.5-00
+sudo apt update
 
 # Install Docker
 sudo apt install docker.io
